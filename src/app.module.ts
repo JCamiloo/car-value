2f95c0be2eb9e35934989e32495acf37dff82fd8
JCamiloo <jcamilo.osorio15@gmail.com>
2022-01-31 18:10:41 -0500

    adding report entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
@@ -12,7 +13,8 @@ import { User } from './users/user.entity';
       type: 'sqlite',
       database: 'db.sqlite',
       entities: [
-        User
+        User,
+        Report
       ],
       synchronize: true,
     }),
